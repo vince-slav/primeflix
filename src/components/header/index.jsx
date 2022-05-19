aed91e112d6297b648cd1c53f6685800e2fec55b
vince-slav <vince-slav@outlook.com>
2022-05-19 20:22:17 -0300

    versao com pagina de detalhes e correcao da logo

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom';
 function Header(){
     return(
         <header>
-            <Link className='logo' to='/' >Logo</Link>
+            <Link className='logo' to='/' >Prime Flix</Link>
             <Link className='favoritos' to='/favoritos' >Meus Filmes</Link>
         </header>
     );
--- src/pages/filme/index.jsx
+++ src/pages/filme/index.jsx
@@ -1,9 +1,63 @@
+import { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom'
+import api from '../../services/api';
+import './index.css'
 
 
 function Filme(){
+    const { id } = useParams();
+    const [filme, setFilme] = useState([]);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        async function loadFilme(){
+            await api.get(`/movie/${id}`, {
+                params: {
+                    api_key: '039d5075b751e99e3ff9e704813708b0',
+                    language: 'pt-BR',
+                }
+            })
+            .then((resp) => {
+                setFilme(resp.data);
+                setLoading(false);
+            })
+            .catch(() => {
+                console.log("errorrrrrrrr");
+            });
+        }
+
+        loadFilme();
+
+        return () => {
+            console.log("Componente desmontado");
+        }
+    }, []);
+
+    if(loading){
+        return(
+            <div className='filme-info'>
+                <h1>Carregando detalhes...</h1>
+            </div>
+        );
+    }
+
+
     return(
-        <div>
-            <h1>Filme</h1>
+        <div className='filme-info'>
+            <h1>{filme.title}</h1>
+            <img src={`https://image.tmdb.org/t/p/original/${filme.backdrop_path}`} alt={filme.title}/>
+            <h3>Sinopse</h3>
+            <span>{filme.overview}</span>
+            <strong>Avaliação: {filme.vote_average} / 10</strong>
+
+            <div className='area-buttons'>
+                <button>Salvar</button>
+                <button>
+                    <a href='#'>
+                        Trailer
+                    </a>
+                </button>
+            </div>
         </div>
     );
 }
